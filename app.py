44a1a029602b4e9923c1466ed084d07791e55ab6
Pedro Ângelo Peregrino e Silva <pedropersonal87@gmail.com>
2023-10-24 13:12:16 -0300

    update finish contatos.html 24-10-2023

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/templates/index.html')
+@app.route('/')
 def program():
     return render_template('index.html')
+
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
 
 
 if __name__ == "__main__":
